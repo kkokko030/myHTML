9b87c5db15c59a6adedf4e09ac0bb280a0aad623
kkokko030 <118319076+kkokko030@users.noreply.github.com>
2026-04-30 16:04:21 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,16 @@
   var y = document.getElementById('year');
   if (y) { y.textContent = new Date().getFullYear(); }
 })();
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+
+    // 버튼 아이콘 변경
+    const btn = document.getElementById("darkBtn");
+
+    if (document.body.classList.contains("dark")) {
+        btn.innerText = "☀️";
+    } else {
+        btn.innerText = "🌙";
+    }
+}
