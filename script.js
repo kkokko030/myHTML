433f729323bb10fd49d07f3462b9752cc4a1ef00
kkokko030 <118319076+kkokko030@users.noreply.github.com>
2026-05-07 15:35:14 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 (function () {
   var y = document.getElementById('year');
   if (y) { y.textContent = new Date().getFullYear(); }
-})();
+});
 
 function toggleDarkMode() {
     document.body.classList.toggle("dark");
@@ -15,4 +15,17 @@ function toggleDarkMode() {
     } else {
         btn.innerText = "🌙";
     }
+
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark'); // 다크모드 저장
+    } else {
+        localStorage.setItem('theme', 'light'); // 라이트모드 저장
+    }
 }
+
+window.onload = () => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.body.classList.add('dark');
+    }
+};
